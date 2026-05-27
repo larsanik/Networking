a53aecd855b6a68a35529a5b5413c19c1a335347
larsanik <larsanik@gmail.com>
2026-05-27 16:54:13 +0300

    Как подключить frontend к приложению время 6:28.

--- frontend/pages/router.py
+++ frontend/pages/router.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
 
+from api.v1.users import get_user
+
 router = APIRouter(
     prefix="/pages",
     tags=["pages"]
@@ -11,4 +13,9 @@ templates = Jinja2Templates(directory="frontend/templates")
 
 @router.get("/index")
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request}) # todo остановился тут
+    return templates.TemplateResponse(request, "index.html" )
+
+
+@router.get("/card/{email}")
+async def get_card(request: Request, user=Depends(get_user)):
+    return templates.TemplateResponse(request, "card.html", {"user": user})
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from api.v1.users import router as users_router
+
+from frontend.pages.router import router as frontend_router
 
 app = FastAPI()
 
@@ -9,8 +12,11 @@ app = FastAPI()
 # async def root():
 #     return {"message": "Hello World! I'm here =o)"}
 
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
 app.include_router(users_router, prefix="/api/v1/users", tags=["users"])
 
+app.include_router(frontend_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
