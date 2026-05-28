6b64a2093ea18c0b0d183d6e00e6f3e09e859751
larsanik <larsanik@gmail.com>
2026-05-28 10:49:34 +0300

    Как подключить frontend к приложению. Завершено.

--- frontend/pages/router.py
+++ frontend/pages/router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
-from starlette.responses import HTMLResponse
 
 from api.v1.users import get_user
 
@@ -13,9 +12,9 @@ templates = Jinja2Templates(directory="frontend/templates")
 
 @router.get("/index")
 async def home(request: Request):
-    return templates.TemplateResponse(request, "index.html" )
+    return templates.TemplateResponse(request=request, name="index.html", context={"request": request})
 
 
 @router.get("/card/{email}")
 async def get_card(request: Request, user=Depends(get_user)):
-    return templates.TemplateResponse(request, "card.html", {"user": user})
+    return templates.TemplateResponse(request=request, name="card.html", context={"request": request, "user": user})
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 
 from api.v1.users import router as users_router
@@ -8,9 +9,9 @@ from frontend.pages.router import router as frontend_router
 
 app = FastAPI()
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World! I'm here =o)"}
+@app.get("/")
+async def root():
+        return RedirectResponse(url="/pages/index")
 
 app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
 
@@ -19,4 +20,4 @@ app.include_router(users_router, prefix="/api/v1/users", tags=["users"])
 app.include_router(frontend_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
