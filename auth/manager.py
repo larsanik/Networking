eb95b3b0f6ae8ae73203221f672384dffc5a7e3f
larsanik <larsanik@gmail.com>
2026-06-03 13:04:05 +0300

    Как создать модели пользователей с помощью класса UserManager.

---
+++ auth/manager.py
@@ -0,0 +1,30 @@
+import uuid
+from typing import Optional
+
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, UUIDIDMixin
+from starlette.requests import Request
+
+from auth.db import User
+from settings import secret
+
+KEY =  secret.reset.get_secret_value()
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = KEY
+    verification_token_secret = KEY
+
+    async def on_after_register(
+        self, user: User, request: Optional[Request] = None
+    ) -> None:
+        print(f"User {user.id} registered")
+
+    async def on_after_forgot_password(
+            self, user: User, token: str, request: Optional[Request] = None
+    ) -> None:
+        print(f"User {user.id} forgot password. Use token to reset: {token}")
+
+    async def on_after_request_verify(
+        self, user: User, token : str, request: Optional[Request] = None
+    ) -> None:
+        print(f"Verification request for {user.id}. Verification token: {token}")
